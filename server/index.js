7b62990a0060a4e10c2bae85302c02cab944c733
Leandro Gamarra <leandrogamarra@rocketmail.com>
2023-06-06 20:40:47 -0400

    se

--- server/index.js
+++ server/index.js
@@ -34,11 +34,17 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 //
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://movie-gram.vercel.app");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "https://movie-gram.vercel.app");
+//   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
-});
+  });
 
 
 
