ebd4e837c7565baab32dcd6d6ba4b332f6b191d3
Leandro Gamarra <leandrogamarra@rocketmail.com>
2023-06-07 21:42:02 -0400

    fs

--- server/index.js
+++ server/index.js
@@ -1604,7 +1604,8 @@ app.get("/api/watchlist/watched_count/:id", (req,res) => {
 //     console.log("running on port 2030");
 // })
 
-app.listen(7964,"0.0.0.0", () => {
+const port = process.env.MYSQLPORT;
+app.listen(port,"0.0.0.0", () => {
     console.log("running on port 7964");
 })
 
