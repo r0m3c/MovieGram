70426df32b8fd79e752808447905b4c7708b46a5
Leandro Gamarra <leandrogamarra@rocketmail.com>
2023-06-08 18:00:28 -0400

    c

--- server/index.js
+++ server/index.js
@@ -64,6 +64,8 @@ const bucketName = process.env.BUCKET_NAME;
 const bucketRegion = process.env.BUCKET_REGION;
 const accessKey = process.env.ACCESS_KEY;
 const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+console.log(bucketRegion);
+cosnole.log(bucketName);
 
 const s3 = new S3Client({
   credentials: {
